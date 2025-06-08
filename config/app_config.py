115588b6fc28fb96686cf901305b3855e79c7773
KiF-D3V <128195346+KiF-D3V@users.noreply.github.com>
2025-06-09 00:20:18 +0200

    Ajout : constantes du projet

---
+++ config/app_config.py
@@ -0,0 +1,20 @@
+# config/app_config.py (version complète)
+
+"""
+Fichier central pour les constantes de configuration de l'application.
+"""
+
+# --- Application Core ---
+APP_NAME = "TrackMyMouse"
+APP_AUTHOR = "TrackMyMouse"
+
+# --- File Names ---
+DB_FILENAME = "stats.db"
+PREFERENCES_FILENAME = "user_preferences.ini"
+
+# --- Stats & Activity Tracking ---
+INACTIVITY_THRESHOLD_SECONDS = 2
+ACTIVITY_TRACKER_INTERVAL = 1
+
+# --- GUI (Graphical User Interface) ---
+HISTORY_DAYS_OPTIONS = [7, 14, 30]
